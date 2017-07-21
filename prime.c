bb641fbc55869f2a52465bb29d8ae1febc411506
Gaya3ram <30343436+Gaya3ram@users.noreply.github.com>
2017-07-21 18:06:11 +0530

    Update and rename .gitignore to prime.c

---
+++ prime.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+int num,i,flag=0;
+printf("enter number");
+scanf("%d",&num);
+{
+for(i=0;i<=n/2;i++)
+{
+{
+if(num%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag=1)
+printf("%d is a prime number",num);
+else
+printf("%d not a prime number",num);
+return 0;
+}
+}
