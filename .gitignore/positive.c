45918a3d626360678cc7b776d03aa150517db532
Gaya3ram <30343436+Gaya3ram@users.noreply.github.com>
2017-07-21 17:50:25 +0530

    Update and rename .gitignore to positive.c

---
+++ .gitignore/positive.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+double no;
+printf("Enter a number");
+scanf("%f",&no);
+if(no<0.0)
+printf("Entered no is negative no");
+else if(num>0.0)
+printf("Entered no is positie no");
+else
+printf("Entered no is Zero");
+return 0;
+}
